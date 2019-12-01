d1eb09d9f197edefe2d56b526a2d954286398053
Mikhail Klimentsyeu <mikhail_klimentsyeu@epam.com>
2019-12-01 17:04:46 +0300

    Add properties

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import model.Client;
 import model.Event;
-import model.EventType;
+import model.EventTypes;
 import service.api.IEventLogger;
 
 public class App {
@@ -14,9 +14,9 @@ public class App {
 
     private IEventLogger defaultLogger;
 
-    private Map<EventType, IEventLogger> eventTypeLoggerMap;
+    private Map<EventTypes, IEventLogger> eventTypeLoggerMap;
 
-    public App(Client client, IEventLogger eventLogger, Map<EventType, IEventLogger> eventTypeLoggerMap) {
+    public App(Client client, IEventLogger eventLogger, Map<EventTypes, IEventLogger> eventTypeLoggerMap) {
         this.client = client;
         this.defaultLogger = eventLogger;
         this.eventTypeLoggerMap = eventTypeLoggerMap;
@@ -27,16 +27,16 @@ public class App {
         App app = (App) ctx.getBean("app");
 
         Event event = ctx.getBean(Event.class);
-        app.logEvent(EventType.INFO, event, "Some event for 1");
+        app.logEvent(EventTypes.INFO, event, "Some event for 1");
 
         event = ctx.getBean(Event.class);
-        app.logEvent(EventType.INFO, event, "One more event for 1");
+        app.logEvent(EventTypes.INFO, event, "One more event for 1");
 
         event = ctx.getBean(Event.class);
-        app.logEvent(EventType.INFO, event, "And one more event for 1");
+        app.logEvent(EventTypes.INFO, event, "And one more event for 1");
 
         event = ctx.getBean(Event.class);
-        app.logEvent(EventType.ERROR, event, "Some event for 2");
+        app.logEvent(EventTypes.ERROR, event, "Some event for 2");
 
         event = ctx.getBean(Event.class);
         app.logEvent(null, event, "Some event for 3");
@@ -44,11 +44,11 @@ public class App {
         ctx.close();
     }
 
-    private void logEvent(EventType eventType, Event event, String msg) {
+    private void logEvent(EventTypes eventTypes, Event event, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
 
-        IEventLogger logger = eventTypeLoggerMap.get(eventType);
+        IEventLogger logger = eventTypeLoggerMap.get(eventTypes);
         if (null == logger) {
             logger = defaultLogger;
         }
